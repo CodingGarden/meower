2df3a0c1e760d6e50d3cf6e8678ba41949285463
w3cj <cj@null.computer>
2020-08-27 08:40:22 -0600

    :sparkles: :package: Update dependencies

--- client/client.js
+++ client/client.js
@@ -5,7 +5,7 @@ const errorElement = document.querySelector('.error-message');
 const loadingElement = document.querySelector('.loading');
 const mewsElement = document.querySelector('.mews');
 const loadMoreElement = document.querySelector('#loadMore');
-const API_URL = window.location.hostname === '127.0.0.1' ? 'http://localhost:5000/v2/mews' : 'https://meower-api.now.sh/v2/mews';
+const API_URL = (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') ? 'http://localhost:5000/v2/mews' : 'https://meower-api.now.sh/v2/mews';
 
 let skip = 0;
 let limit = 5;
